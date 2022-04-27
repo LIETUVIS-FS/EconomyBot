6fe25bb16b648a6159524e44e7b08ff5d8feac05
LIETUVIS-FS <89350146+LIETUVIS-FS@users.noreply.github.com>
2022-04-27 09:15:26 +0300

    Update botConfig.js

--- botConfig.js
+++ botConfig.js
@@ -1,8 +1,8 @@
 module.exports = {
-    token: "YOUR_TOKEN",
+    token: "OTU3OTQwNjMwMTM3NDg3NDQw.YkGFuw.Em9FYsj_H2NMw_QqS8AHpbhzZu0",
     prefix: "PREFIX",
     admins: [
-        "PEOPLE WHO CAN USE ADD MONEY (IDS)"
+        "PEOPLE WHO CAN USE ADD MONEY (821722657778171995)"
 ],
     debug: true,
     countChannel: "countChannelID"
